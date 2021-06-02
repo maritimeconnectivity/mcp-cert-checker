f3cdb333acd41f3e4fd870ae7490be9537e2af21
Oliver Haagh <ohaagh@gmail.com>
2021-06-02 16:18:08 +0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const pemTextArea = document.getElementById('certTextArea');
+const pemFileUploader = document.getElementById('certFileUploader');
+const submitButton = document.getElementById('submitBtn');
