33d6ae723327c45481550ff55c2aec5dea0fc852
Oliver Haagh <ohaagh@gmail.com>
2021-06-03 16:26:24 +0200

    Some more progress

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,9 @@ const caFileUploader: HTMLInputElement = document.getElementById('caCertFileUplo
 
 const submitButton: HTMLButtonElement = document.getElementById('submitBtn') as HTMLButtonElement;
 
-let cert, subCaCert, caCert: string | ArrayBuffer;
+let cert: string | ArrayBuffer;
+let subCaCert: string | ArrayBuffer;
+let caCert: string | ArrayBuffer;
 
 pemTextArea.addEventListener("input", () => {
    cert = pemTextArea.value;
@@ -20,7 +22,7 @@ pemTextArea.addEventListener("input", () => {
 
 certFileUploader.addEventListener("input", async () => {
     if (certFileUploader.files.length > 0) {
-        let file = certFileUploader.files[0];
+        const file = certFileUploader.files[0];
         cert = await file.arrayBuffer();
     }
 });
@@ -31,7 +33,7 @@ subCaPemTextArea.addEventListener("input", () => {
 
 subCaFileUploader.addEventListener("input", async () => {
     if (subCaFileUploader.files.length > 0) {
-        let file = subCaFileUploader.files[0];
+        const file = subCaFileUploader.files[0];
         subCaCert = await file.arrayBuffer();
     }
 });
@@ -42,7 +44,18 @@ caPemTextArea.addEventListener("input", () => {
 
 caFileUploader.addEventListener("input", async () => {
    if (caFileUploader.files.length > 0) {
-       let file = caFileUploader.files[0];
+       const file = caFileUploader.files[0];
        caCert = await file.arrayBuffer();
+   }
+});
+
+submitButton.addEventListener("click", () => {
+   if (cert) {
+       if (typeof cert === "string") {
+           const b64 = cert.replace(/(-----(BEGIN|END) CERTIFICATE-----|[\n\r])/g, '');
+           const bytes = atob(b64);
+       } else if (cert instanceof ArrayBuffer) {
+
+       }
    }
 });
