02668cd458cca1abb50f7b1aa2f306f190d5ec54
Oliver Haagh <oliver@dmc.international>
2021-06-04 16:05:46 +0200

    We can now parse a pem encoded certificate

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {fromBER} from "asn1js";
+import Certificate from "pkijs/src/Certificate";
 
 const pemTextArea: HTMLTextAreaElement = document.getElementById('certTextArea') as HTMLTextAreaElement;
 const certFileUploader: HTMLInputElement = document.getElementById('certFileUploader') as HTMLInputElement;
@@ -11,8 +13,9 @@ const caPemTextArea: HTMLTextAreaElement = document.getElementById('caCertTextAr
 const caFileUploader: HTMLInputElement = document.getElementById('caCertFileUploader') as HTMLInputElement;
 
 const submitButton: HTMLButtonElement = document.getElementById('submitBtn') as HTMLButtonElement;
+const clearButton: HTMLButtonElement = document.getElementById('clearBtn') as HTMLButtonElement;
 
-let cert: string | ArrayBuffer;
+let cert: string;
 let subCaCert: string | ArrayBuffer;
 let caCert: string | ArrayBuffer;
 
@@ -23,7 +26,22 @@ pemTextArea.addEventListener("input", () => {
 certFileUploader.addEventListener("input", async () => {
     if (certFileUploader.files.length > 0) {
         const file = certFileUploader.files[0];
-        cert = await file.arrayBuffer();
+        let content = await file.text();
+        let matches = [...content.matchAll(/(-----BEGIN CERTIFICATE-----)(.*?)(-----END CERTIFICATE-----)/smg)];
+        if (matches.length > 0) {
+            cert = matches[0][0];
+            pemTextArea.value = cert;
+            if (matches.length > 1) {
+                subCaCert = matches[1][0];
+                subCaPemTextArea.value = subCaCert;
+                if (matches.length > 2) {
+                    caCert = matches[2][0];
+                    caPemTextArea.value = caCert;
+                }
+            }
+        } else {
+            alert('Certificate must be PEM encoded!');
+        }
     }
 });
 
@@ -51,11 +69,16 @@ caFileUploader.addEventListener("input", async () => {
 
 submitButton.addEventListener("click", () => {
    if (cert) {
-       if (typeof cert === "string") {
-           const b64 = cert.replace(/(-----(BEGIN|END) CERTIFICATE-----|[\n\r])/g, '');
-           const bytes = atob(b64);
-       } else if (cert instanceof ArrayBuffer) {
-
-       }
+       const b64 = cert.replace(/(-----(BEGIN|END) CERTIFICATE-----|[\n\r])/g, '');
+       const binary = atob(b64);
+       const bytes = Uint8Array.from(binary, c => c.charCodeAt(0));
+       //console.log(bytes);
+       const asn1 = fromBER(bytes.buffer);
+       const certificate = new Certificate({schema: asn1.result});
+       //console.log(certificate);
    }
+});
+
+clearButton.addEventListener("click", () => {
+   location.reload();
 });
