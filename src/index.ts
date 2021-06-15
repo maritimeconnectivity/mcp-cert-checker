a3b7bb748a8758d4743778c8468124d2ae4ee325
Oliver Haagh <oliver@dmc.international>
2021-06-15 10:16:24 +0200

    Current progress src/types/index.d.ts contains updated type definitions for PKI.js

--- src/index.ts
+++ src/index.ts
@@ -2,83 +2,109 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {fromBER} from "asn1js";
 import Certificate from "pkijs/src/Certificate";
+import CertificateChainValidationEngine from "pkijs/src/CertificateChainValidationEngine";
+import OCSPRequest from "pkijs/src/OCSPRequest";
 
-const pemTextArea: HTMLTextAreaElement = document.getElementById('certTextArea') as HTMLTextAreaElement;
 const certFileUploader: HTMLInputElement = document.getElementById('certFileUploader') as HTMLInputElement;
-
-const subCaPemTextArea: HTMLTextAreaElement = document.getElementById('subCaCertTextArea') as HTMLTextAreaElement;
 const subCaFileUploader: HTMLInputElement = document.getElementById('subCaCertFileUploader') as HTMLInputElement;
-
-const caPemTextArea: HTMLTextAreaElement = document.getElementById('caCertTextArea') as HTMLTextAreaElement;
 const caFileUploader: HTMLInputElement = document.getElementById('caCertFileUploader') as HTMLInputElement;
 
 const submitButton: HTMLButtonElement = document.getElementById('submitBtn') as HTMLButtonElement;
+const checkOCSPButton: HTMLButtonElement = document.getElementById('ocspBtn') as HTMLButtonElement;
 const clearButton: HTMLButtonElement = document.getElementById('clearBtn') as HTMLButtonElement;
 
-let cert: string;
-let subCaCert: string | ArrayBuffer;
-let caCert: string | ArrayBuffer;
+const textAreas: Array<HTMLTextAreaElement> =
+    [
+        document.getElementById('certTextArea') as HTMLTextAreaElement,
+        document.getElementById('subCaCertTextArea') as HTMLTextAreaElement,
+        document.getElementById('caCertTextArea') as HTMLTextAreaElement
+    ];
 
-pemTextArea.addEventListener("input", () => {
-   cert = pemTextArea.value;
+const certs: Array<string> = new Array<string>(3);
+
+textAreas[0].addEventListener("input", () => {
+   const content = textAreas[0].value;
+   if (content.length > 0) {
+       extractCerts(content);
+   }
 });
 
 certFileUploader.addEventListener("input", async () => {
     if (certFileUploader.files.length > 0) {
         const file = certFileUploader.files[0];
         let content = await file.text();
-        let matches = [...content.matchAll(/(-----BEGIN CERTIFICATE-----)(.*?)(-----END CERTIFICATE-----)/smg)];
-        if (matches.length > 0) {
-            cert = matches[0][0];
-            pemTextArea.value = cert;
-            if (matches.length > 1) {
-                subCaCert = matches[1][0];
-                subCaPemTextArea.value = subCaCert;
-                if (matches.length > 2) {
-                    caCert = matches[2][0];
-                    caPemTextArea.value = caCert;
-                }
-            }
-        } else {
-            alert('Certificate must be PEM encoded!');
-        }
+        extractCerts(content);
     }
 });
 
-subCaPemTextArea.addEventListener("input", () => {
-   subCaCert = subCaPemTextArea.value;
+textAreas[1].addEventListener("input", () => {
+   certs[1] = textAreas[1].value;
 });
 
 subCaFileUploader.addEventListener("input", async () => {
     if (subCaFileUploader.files.length > 0) {
         const file = subCaFileUploader.files[0];
-        subCaCert = await file.arrayBuffer();
+        const content = await file.text();
+        certs[1] = content;
+        textAreas[1].value = content;
     }
 });
 
-caPemTextArea.addEventListener("input", () => {
-   caCert = caPemTextArea.value;
+textAreas[2].addEventListener("input", () => {
+    certs[2] = textAreas[2].value;
 });
 
 caFileUploader.addEventListener("input", async () => {
    if (caFileUploader.files.length > 0) {
        const file = caFileUploader.files[0];
-       caCert = await file.arrayBuffer();
+       const content = await file.text();
+       certs[2] = content;
+       textAreas[2].value = content;
    }
 });
 
 submitButton.addEventListener("click", () => {
-   if (cert) {
-       const b64 = cert.replace(/(-----(BEGIN|END) CERTIFICATE-----|[\n\r])/g, '');
-       const binary = atob(b64);
-       const bytes = Uint8Array.from(binary, c => c.charCodeAt(0));
-       //console.log(bytes);
-       const asn1 = fromBER(bytes.buffer);
-       const certificate = new Certificate({schema: asn1.result});
-       //console.log(certificate);
-   }
+    const parsedCerts: Array<Certificate> = certs.map(c => parseCertificate(c));
+    console.log(parsedCerts);
+    const validationEngine: CertificateChainValidationEngine = new CertificateChainValidationEngine(
+        {
+            certs: parsedCerts.slice(0, parsedCerts.length - 1),
+            trustedCerts: [parsedCerts[parsedCerts.length - 1]]
+        });
+    console.log(validationEngine);
+    validationEngine.verify().then(r => {
+        if (r?.result) {
+            alert("The trust chain was successfully verified!");
+        } else {
+            alert("The trust chain could not be verified!");
+        }
+    }, () => alert("This was bad!"));
+});
+
+checkOCSPButton.addEventListener("click", async () => {
+    const parsedCerts: Array<Certificate> = certs.map(parseCertificate);
+    const ocspReq: OCSPRequest = new OCSPRequest();
+
+    await ocspReq.createForCertificate(parsedCerts[0], {hashAlgorithm: "SHA-384", issuerCertificate: parsedCerts[1]});
+    console.log(ocspReq);
 });
 
 clearButton.addEventListener("click", () => {
    location.reload();
 });
+
+function parseCertificate(pemCert: string): Certificate {
+    const b64 = pemCert.replace(/(-----(BEGIN|END) CERTIFICATE-----|[\n\r])/g, '');
+    const binary = atob(b64);
+    const bytes = Uint8Array.from(binary, c => c.charCodeAt(0));
+    const asn1 = fromBER(bytes.buffer);
+    return new Certificate({schema: asn1.result});
+}
+
+function extractCerts(pemCerts: string): void {
+    let matches = [...pemCerts.matchAll(/(-----BEGIN CERTIFICATE-----)(.*?)(-----END CERTIFICATE-----)/smg)];
+    matches.forEach((m, i) => {
+       certs[i] = m[0];
+       textAreas[i].value = certs[i];
+    });
+}
