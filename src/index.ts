3a4e9050e5d4927096ab6b665a1a86247700dfee
Oliver Haagh <oliver@dmc.international>
2021-06-16 16:09:21 +0200

    Can now also check CRL and also both CRL and OCSP are included in trust chain verification.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Asn1js, {fromBER} from "asn1js";
+import Asn1js, {fromBER, LocalBaseBlock} from "asn1js";
 import Certificate from "pkijs/src/Certificate";
 import CertificateChainValidationEngine from "pkijs/src/CertificateChainValidationEngine";
 import OCSPRequest from "pkijs/src/OCSPRequest";
 import InfoAccess from "pkijs/src/InfoAccess";
 import OCSPResponse from "pkijs/src/OCSPResponse";
+import CRLDistributionPoints from "pkijs/src/CRLDistributionPoints";
+import CertificateRevocationList from "pkijs/src/CertificateRevocationList";
+
+interface Asn1Struct {
+    offset: number,
+    result: LocalBaseBlock
+}
 
 const certFileUploader: HTMLInputElement = document.getElementById('certFileUploader') as HTMLInputElement;
 const subCaFileUploader: HTMLInputElement = document.getElementById('subCaCertFileUploader') as HTMLInputElement;
@@ -14,6 +21,7 @@ const caFileUploader: HTMLInputElement = document.getElementById('caCertFileUplo
 const submitButton: HTMLButtonElement = document.getElementById('submitBtn') as HTMLButtonElement;
 const checkOCSPButton: HTMLButtonElement = document.getElementById('ocspBtn') as HTMLButtonElement;
 const clearButton: HTMLButtonElement = document.getElementById('clearBtn') as HTMLButtonElement;
+const crlButton: HTMLButtonElement = document.getElementById('crlBtn') as HTMLButtonElement;
 
 const textAreas: Array<HTMLTextAreaElement> =
     [
@@ -65,12 +73,16 @@ caFileUploader.addEventListener("input", async () => {
    }
 });
 
-submitButton.addEventListener("click", () => {
+submitButton.addEventListener("click", async () => {
     const parsedCerts: Array<Certificate> = certs.map(c => parseCertificate(c));
+    const ocspResponse = await getOCSP(parsedCerts[0], parsedCerts[1]);
+    const crls = await Promise.all(parsedCerts.slice(0,2).map(getCRL));
     const validationEngine: CertificateChainValidationEngine = new CertificateChainValidationEngine(
         {
             certs: parsedCerts.slice(0, parsedCerts.length - 1),
-            trustedCerts: [parsedCerts[parsedCerts.length - 1]]
+            trustedCerts: [parsedCerts[2]],
+            ocsps: [ocspResponse],
+            crls: crls
         });
     validationEngine.verify().then(r => {
         if (r?.result) {
@@ -83,24 +95,8 @@ submitButton.addEventListener("click", () => {
 
 checkOCSPButton.addEventListener("click", async () => {
     const parsedCerts: Array<Certificate> = certs.map(parseCertificate);
-    const ocspReq: OCSPRequest = new OCSPRequest();
 
-    await ocspReq.createForCertificate(parsedCerts[0], {hashAlgorithm: "SHA-384", issuerCertificate: parsedCerts[1]});
-    const ocsp = ocspReq.toSchema(true) as Asn1js.Sequence;
-    const tmp = parsedCerts[0].extensions.filter(e => e.extnID === "1.3.6.1.5.5.7.1.1")[0].parsedValue as InfoAccess;
-    const ocspUrl = tmp.accessDescriptions[0].accessLocation.value;
-    const response = await fetch(ocspUrl, {
-        method: 'POST',
-        mode: "cors",
-        cache: "no-cache",
-        headers: {
-            'Content-Type': 'application/ocsp-request'
-        },
-        body: ocsp.toBER()
-    });
-    const rawOcspResponse = await (await response.blob()).arrayBuffer();
-    const asn1 = fromBER(rawOcspResponse);
-    const ocspResponse = new OCSPResponse({schema: asn1.result});
+    const ocspResponse = await getOCSP(parsedCerts[0], parsedCerts[1]);
     const status = await ocspResponse.getCertificateStatus(parsedCerts[0], parsedCerts[1]);
 
     let message;
@@ -118,15 +114,35 @@ checkOCSPButton.addEventListener("click", async () => {
     alert(message);
 });
 
+crlButton.addEventListener("click", async () => {
+    const parsedCerts: Array<Certificate> = certs.map(parseCertificate);
+    console.log(parsedCerts);
+
+    const crl = await getCRL(parsedCerts[0]);
+    if (await crl.verify({issuerCertificate: parsedCerts[1]})) {
+        if (crl.isCertificateRevoked(parsedCerts[0])) {
+            alert("The certificate has been revoked.");
+        } else {
+            alert("The certificate is valid.");
+        }
+    } else {
+        alert("The CRL could not be verified.");
+    }
+});
+
 clearButton.addEventListener("click", () => {
    location.reload();
 });
 
-function parseCertificate(pemCert: string): Certificate {
-    const b64 = pemCert.replace(/(-----(BEGIN|END) CERTIFICATE-----|[\n\r])/g, '');
+function parsePem(input: string): Asn1Struct {
+    const b64 = input.replace(/(-----(BEGIN|END) (.*?)-----|[\n\r])/g, '');
     const binary = atob(b64);
     const bytes = Uint8Array.from(binary, c => c.charCodeAt(0));
-    const asn1 = fromBER(bytes.buffer);
+    return fromBER(bytes.buffer);
+}
+
+function parseCertificate(pemCert: string): Certificate {
+    const asn1 = parsePem(pemCert);
     return new Certificate({schema: asn1.result});
 }
 
@@ -136,4 +152,38 @@ function extractCerts(pemCerts: string): void {
        certs[i] = m[0];
        textAreas[i].value = certs[i];
     });
+}
+
+async function getOCSP(certificate: Certificate, issuerCertificate: Certificate): Promise<OCSPResponse> {
+    const ocspReq: OCSPRequest = new OCSPRequest();
+
+    await ocspReq.createForCertificate(certificate, {hashAlgorithm: "SHA-384", issuerCertificate: issuerCertificate});
+    const ocsp = ocspReq.toSchema(true) as Asn1js.Sequence;
+    const tmp = certificate.extensions.filter(e => e.extnID === "1.3.6.1.5.5.7.1.1")[0].parsedValue as InfoAccess;
+    const ocspUrl = tmp.accessDescriptions[0].accessLocation.value;
+    const response = await fetch(ocspUrl, {
+        method: 'POST',
+        mode: "cors",
+        cache: "no-cache",
+        headers: {
+            'Content-Type': 'application/ocsp-request'
+        },
+        body: ocsp.toBER()
+    });
+    const rawOcspResponse = await (await response.blob()).arrayBuffer();
+    const asn1 = fromBER(rawOcspResponse);
+    return new OCSPResponse({schema: asn1.result});
+}
+
+async function getCRL(certificate: Certificate): Promise<CertificateRevocationList> {
+    const crlExt = certificate.extensions.filter(e => e.extnID === '2.5.29.31')[0].parsedValue as CRLDistributionPoints;
+    const crlUrl = crlExt.distributionPoints[0].distributionPoint[0].value as string;
+
+    const response = await fetch(crlUrl, {
+        mode: "cors",
+        cache: "no-cache"
+    });
+    const crlString = await response.text();
+    const crlAsn1 = parsePem(crlString);
+    return new CertificateRevocationList({schema: crlAsn1.result});
 }
