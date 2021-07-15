460cb963a740bd1b651e5ee7a6144bd58929c747
Oliver Haagh <ohaagh@gmail.com>
2021-07-15 15:10:45 +0200

    Add source map to prod build

--- webpack.config.js
+++ webpack.config.js
@@ -56,6 +56,7 @@ const config = {
 module.exports = () => {
   if (isProduction) {
     config.mode = "production";
+    config.devtool = "source-map";
   } else {
     config.mode = "development";
   }
